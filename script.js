90a3c3d6e914325a606da9899d78ef06be48e637
jbaldwin0 <jac0b67@ymail.com>
2022-07-20 19:59:32 -0500

    Add playRound function

--- script.js
+++ script.js
@@ -1,14 +1,36 @@
 
 function getComputerChoice() {
-    compChoice = parseInt(Math.floor(Math.random()*(9-1+1)+1));
-    console.log(compChoice);
-    if (compChoice <= 3) {
+    compChoice = parseInt(Math.floor(Math.random()*(3-1+1)+1));
+
+    if (compChoice <= 1) {
         return "Rock";
-    } else if (compChoice <= 6) {
+    } else if (compChoice <= 2) {
         return "Paper";
     } else {
         return "Scissors";
     }
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if (playerSelection == 'ROCK' && computerSelection == 'PAPER') {
+        return 'lose';
+    } else if (playerSelection == 'ROCK' && computerSelection == 'SCISSORS') {
+        return 'win';
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'ROCK') {
+        return 'lose';
+    } else if (playerSelection == 'SCISSORS' && computerSelection == 'PAPER') {
+        return 'win';
+    } else if (playerSelection == 'PAPER' && computerSelection == 'SCISSORS') {
+        return 'lose';
+    } else if (playerSelection == 'PAPER' && computerSelection == 'ROCK') {
+        return 'win';
+    } else {
+        return 'tie';
+    }
+}
+
+console.log(playRound('scissors', getComputerChoice()));
